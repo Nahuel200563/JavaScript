b16c7a6d2cf908fc4a2945c81baa007345ab5b13
nahuel castillo <castillonahuel537@gmail.com>
2024-10-24 15:49:32 -0300

    entrega 2

--- index.js
+++ index.js
@@ -1,41 +1,86 @@
+const alumnos = [];
 
+class Alumno {
+  constructor(nombre, notaUno, notaDos, notaTres) {
+    this.nombre = nombre;
+    this.notaUno = notaUno;
+    this.notaDos = notaDos;
+    this.notaTres = notaTres;
+  }
+  calculadoraDePromedios = () => {
+    return (this.notaUno + this.notaDos + this.notaTres) / 3;
+  };
+}
 
-let nombreApellido1 = (
-    prompt("ingresa el Nombre y Apellido del Primer Alumno") );
-    while (nombreApellido1 === "" || !isNaN(nombreApellido1)) {
+const cantidadDeAlumnos = parseFloat(prompt(`Ingresa la cantidad de alumnos`));
+
+for (let i = 0; i < cantidadDeAlumnos; i++) {
+  let nombreApellido1 = prompt(
+    "Ingresa el Nombre y Apellido del Alumno " + (i + 1)
+  );
+  while (true) {
+    if (nombreApellido1 === null) {
+      alert(`Proceso cancelado`);
+      break;
+    } else {
+      while (nombreApellido1 === "" || !isNaN(nombreApellido1)) {
         alert("Por favor ingrese un nombre válido (no vacío y no un número)");
-        nombreApellido1 = prompt("Ingresa el Nombre y Apellido del Primer Alumno");
+        nombreApellido1 = prompt(
+          `Ingresa el Nombre y Apellido del Alumno ${i + 1}`
+        );
+      }
+      let notaUno = parseFloat(
+        prompt(`Ingresa la primera nota del alumno ${i + 1}`)
+      );
+      while (isNaN(notaUno) || notaUno < 0 || notaUno > 10) {
+        alert("Por favor ingrese un número válido del 0 al 10");
+        notaUno = parseFloat(
+          prompt(`Ingresa la primera nota del alumno ${i + 1}`)
+        );
+      }
+      let notaDos = parseFloat(
+        prompt(`Ingresa la segunda nota del alumno ${i + 1}`)
+      );
+      while (isNaN(notaDos) || notaDos < 0 || notaDos > 10) {
+        alert("Por favor ingrese un número válido del 0 al 10");
+        notaDos = parseFloat(
+          prompt(`Ingresa la segunda nota del alumno ${i + 1}`)
+        );
+      }
+      let notaTres = parseFloat(
+        prompt(`Ingresa la tercera nota del alumno ${i + 1}`)
+      );
+      while (isNaN(notaTres) || notaTres < 0 || notaTres > 10) {
+        alert("Por favor ingrese un número válido del 0 al 10");
+        notaTres = parseFloat(
+          prompt(`Ingresa la tercera nota del alumno ${i + 1}`)
+        );
+      }
+
+      let nuevoAlumno = new Alumno(nombreApellido1, notaUno, notaDos, notaTres);
+      alumnos.push(nuevoAlumno);
+
+      let promedioAlumno = nuevoAlumno.calculadoraDePromedios();
+      console.log(
+        `El promedio de ${nombreApellido1} es ${promedioAlumno.toFixed(2)}`
+      );
+
+      if (promedioAlumno < 7) {
+        alert(`El Alumno ${nombreApellido1} desaprobó`);
+      } else {
+        alert(`El Alumno ${nombreApellido1} aprobó`);
+      }
+      break;
     }
+  }
+}
 
-    let notaUno = (
-        parseFloat( prompt("ingresa la primera nota del alumno n1")));
-    while (notaUno === "" || isNaN(notaUno) || notaUno > 10 || 0 >= notaUno) {
-        alert ("Porfavor ingrese un numero valido del 1 al 10")
-        notaUno = parseFloat ( prompt("ingresa la primera nota del alumno n1"))
-    }
-
-    let notaDos = (
-        parseFloat( prompt("ingresa la Segunda nota del alumno n1")));
-    while (notaDos === "" || isNaN(notaDos) || notaDos > 10 || 0 >= notaDos) {
-        alert ("Porfavor ingrese un numero valido del 1 al 10")
-        notaDos = parseFloat ( prompt("ingresa la segunda nota del alumno n1"))
-    }
-
-    let notaTres = (
-        parseFloat( prompt("ingresa la tercera nota del alumno n1")));
-    while (notaTres === "" || isNaN(notaTres) || notaTres > 10 || 0 >= notaTres) {
-        alert ("Porfavor ingrese un numero valido del 1 al 10")
-        notaTres = parseFloat ( prompt("ingresa la Tercera nota del alumno n1"))
-    }
-        
-promedioAlumno1 = (notaUno + notaDos + notaTres) / 3 ;  
-promedioAlumno1 = promedioAlumno1.toFixed(4);
-console.log( "El promedio de " + nombreApellido1 + " es " + promedioAlumno1  );
-
-
-
-
-
-
-
-
+console.table(alumnos);
+const aprobados = alumnos.filter(
+  (alumnos) => alumnos.calculadoraDePromedios() >= 7
+);
+const desaprobados = alumnos.filter(
+  (alumnos) => alumnos.calculadoraDePromedios() < 7
+);
+console.log(`Los alumnos que aprobaron son `, aprobados);
+console.log(`Los alumnos desaprobados son`, desaprobados);
